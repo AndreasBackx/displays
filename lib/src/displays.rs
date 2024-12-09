0e64653f3ac81bab94d7203f2ca05acfb0e78ca9
Andreas Backx <andreas@backx.org>
2024-12-09 21:46:13 +0000

    Apply return vector of unused updates.

--- lib/src/displays.rs
+++ lib/src/displays.rs
@@ -62,7 +62,7 @@ impl Displays {
         })
     }
 
-    fn apply(updates: Vec<DisplayUpdate>, validate: bool) -> anyhow::Result<()> {
+    fn apply(updates: Vec<DisplayUpdate>, validate: bool) -> anyhow::Result<Vec<DisplayUpdate>> {
         let ids = updates.iter().map(|update| &update.id).collect();
         let mut id_mapping = Self::get_inner(ids)?;
         let updates_inner: Vec<_> = updates
@@ -83,20 +83,47 @@ impl Displays {
             .into_iter()
             .filter_map(|display| display.into())
             .collect();
-        LogicalDisplayManagerWindows::apply(logical_updates, validate)?;
+        let remaining_logical_updates =
+            LogicalDisplayManagerWindows::apply(logical_updates, validate)?;
         let physical_updates: Vec<PhysicalDisplayUpdate> = updates_inner
             .into_iter()
             .filter_map(|display| display.into())
             .collect();
-        PhysicalDisplayManagerWindows::apply(physical_updates)?;
-        Ok(())
+        let mut remaining_physical_updates =
+            PhysicalDisplayManagerWindows::apply(physical_updates)?;
+
+        let remaining_updates = remaining_logical_updates
+            .into_iter()
+            .map(|logical_update| DisplayUpdate {
+                id: logical_update.id.outer.clone(),
+                logical: Some(logical_update.content),
+                physical: remaining_physical_updates
+                    .iter()
+                    .position(|physical_update| physical_update.id == logical_update.id)
+                    .map(|index| remaining_physical_updates.remove(index).content),
+            })
+            // Collecting is done tomake sure `remaining_physical_updates` has had all of its
+            // matching items removed.
+            .collect::<Vec<_>>()
+            .into_iter()
+            .chain(
+                remaining_physical_updates
+                    .into_iter()
+                    .map(|physical_update| DisplayUpdate {
+                        id: physical_update.id.outer,
+                        physical: Some(physical_update.content),
+                        logical: None,
+                    }),
+            )
+            .collect();
+        Ok(remaining_updates)
     }
 
-    pub fn update(updates: Vec<DisplayUpdate>) -> anyhow::Result<()> {
+    pub fn update(updates: Vec<DisplayUpdate>) -> anyhow::Result<Vec<DisplayUpdate>> {
         Self::apply(updates, false)
     }
 
-    pub fn validate(updates: Vec<DisplayUpdate>) -> anyhow::Result<()> {
+    pub fn validate(updates: Vec<DisplayUpdate>) -> anyhow::Result<Vec<DisplayUpdate>> {
         Self::apply(updates, true)
     }
 }
