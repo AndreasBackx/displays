1ee139d73c62d0950aca684c62b40791a132b09e
Andreas Backx <andreas@backx.org>
2024-12-18 23:06:49 +0000

    stash

---
+++ py/examples/everything.py
@@ -0,0 +1,27 @@
+import asyncio
+import sys
+
+from displays import *
+
+
+async def main() -> None:
+    print(query())
+
+    ids = [display.id for display in query()]
+    up_brightness_updates = [
+        DisplayUpdate(id=id, physical=PhysicalDisplayUpdateContent(brightness=50))
+        for id in ids
+    ]
+    apply(up_brightness_updates)
+
+    down_brightness_updates = [
+        DisplayUpdate(id=id, physical=PhysicalDisplayUpdateContent(brightness=0))
+        for id in ids
+    ]
+    apply(down_brightness_updates)
+
+    await asyncio.sleep(1)
+
+
+if __name__ == "__main__":
+    sys.exit(asyncio.run(main()))
